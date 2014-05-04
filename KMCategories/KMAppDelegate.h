d4626bc83c7c1883fc34be4b5a4ebad10ff674cf
K <mustkre8@gmail.com>
2014-05-04 09:29:12 -0400

    Initial Commit

---
+++ KMCategories/KMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KMAppDelegate.h
+//  KMCategories
+//
+//  Created by Khalid on 5/4/14.
+//  Copyright (c) 2014 iksnae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KMCategories/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KMCategories
+//
+//  Created by Khalid on 5/4/14.
+//  Copyright (c) 2014 iksnae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KMAppDelegate class]));
+    }
+}
